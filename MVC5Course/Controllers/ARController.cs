016572e18d61f70eac147232858b175bbbf1d036
Aaronlivy <Aaronlivy@DESKTOP-BIUF8GJ>
2017-03-18 14:06:22 +0800

    練習使用 FileResult 回傳任意檔案

---
+++ MVC5Course/Controllers/ARController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    public class ARController : Controller
+    {
+        // GET: AR
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult View2()
+        {
+            return PartialView("Index");
+        }
+
+        public ActionResult View3()
+        {
+            return View();
+        }
+
+        public ActionResult File1()
+        {
+            return File(Server.MapPath("~/Content/Images/10752579.png"), "image/png");
+        }
+
+        public ActionResult File2()
+        {
+            return File(Server.MapPath("~/Content/Images/10752579.png"), "image/png", "白爛貓.jpg");
+        }
+    }
+}
